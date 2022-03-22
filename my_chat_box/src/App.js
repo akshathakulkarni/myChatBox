751c0c129c8b339aad4bdcafdc907b73eda90dfb
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-03-22 14:39:49 -0400

    Render the ChatFeed component and pass down chatAppProps obtained from ChatEngine

--- my_chat_box/src/App.js
+++ my_chat_box/src/App.js
@@ -1,15 +1,17 @@
 import { ChatEngine } from "react-chat-engine";
 
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 const App = () => {
   return (
-    //projectID, userName and userSecret values used from chatengine.io
+    //projectID, userName and userSecret values used from chatengine.io - admin user
     <ChatEngine 
     height="100vh"
     projectID="5bb130f8-c18a-430a-bcc8-3260644715a0"
     userName="jsbuddies"
-    userSecret="12345"/>
+    userSecret="12345"
+    renderChatFeed={ (chatAppProps) => <ChatFeed {...chatAppProps} />  } />
   );
 };
 
