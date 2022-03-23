5d9a420ec5e631dd22c8a7a66a386c7c334e66c0
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-03-23 13:49:07 -0400

    Create ChatFeed component and a function to renderMessages

---
+++ my_chat_box/src/components/ChatFeed.jsx
@@ -0,0 +1,40 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+const ChatFeed = (props) => {
+  const { chats, activeChat, userName, messages } = props;
+  const chat = chats && chats[activeChat];
+  console.log(chat, userName, messages);
+  const renderMessages = () => {
+    const keys = Object.keys(messages); //gives the ids of all messages
+    const key = keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessage = index === 0 ? null : keys[index - 1]; 
+      const isMyMessage = userName === message.sender.userName; 
+      return (
+        <div key={`msg_${index}`}>
+          <div className="message-block">
+            {
+              isMyMessage 
+              ? <MyMessage />
+              : <TheirMessage />
+            }
+          </div>
+          <div className="read-receipts" style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+            read-receipts
+          </div>
+
+        </div>
+      )
+    })
+  }
+  renderMessages();
+  return (
+    <div>
+      ChatFeed
+    </div>
+  );
+};
+
+export default ChatFeed;
